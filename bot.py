9f0d7c9362ca4f075be5bf4553527aca6f0843ba
TorNATO-TAO <nathan.waltz@wsu.edu>
2021-03-06 12:47:46 -0800

    Added the functionality so that when you ping the bot, it sends you a direct message

--- bot.py
+++ bot.py
@@ -1,22 +1,34 @@
-import asyncio
-import discord
 import os
 from dotenv import load_dotenv
+from discord import Client
+from discord import Game
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = discord.Client()
+client = Client()
+
+
+@client.event
+async def on_ready():
+    print('Currently logged in as:')
+    print(client.user.name)
+    print(client.user.id)
+    print('Guilds that I am currently a member of')
+    for guild in client.guilds:
+        print(guild)
+
+    await client.change_presence(activity=Game('Chilling'))
+
 
 @client.event
 async def on_message(message):
-    
-    # Avoid replying to it's own messages
-    if(message.author.bot):
+    if message.author.bot:
         return
-    
-    # Wait for a user to send the first message
-    if(message.content):
-        await message.channel.send("Hello, I am a bot. React with a thumbs up to confirm that you want to use this service.")    
-        
+
+    # checks if the bot has been @ed
+    if message.content == f'<@!{client.user.id}>':
+        await message.author.send('Hi, I am ...')
+        await message.channel.send(f'<@!{message.author.id}> Check your DM\'s')
+
 client.run(TOKEN)
