345fb22ea8a1a7bfc25332722072da4078f8f86c
Harrison <harrison.greenlee@wsu.edu>
2021-03-06 11:07:06 -0800

    Fixed self-replies

--- bot.py
+++ bot.py
@@ -10,7 +10,13 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
+    
+    # Avoid replying to it's own messages
+    if(message.author.bot):
+        return
+    
+    # Wait for a user to send the first message
     if(message.content):
-        await message.channel.send("Hello, I am a bot. React with a thumbs up to confirm that you want to use this service.")
+        await message.channel.send("Hello, I am a bot. React with a thumbs up to confirm that you want to use this service.")    
         
 client.run(TOKEN)
