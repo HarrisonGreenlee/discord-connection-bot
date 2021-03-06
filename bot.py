ca4e3cbe5a0a7c2e5cf06d786495c3a7c3fcc6a1
TorNATO-TAO <nathan.waltz@wsu.edu>
2021-03-06 13:15:39 -0800

    Trying to react to my own messages

--- bot.py
+++ bot.py
@@ -22,18 +22,15 @@ async def on_ready():
 
 
 @client.event
-async def on_ready():
-    print("Successfully connected to discord.")
-
-
-@client.event
 async def on_message(message):
     if message.author.bot:
         return
 
     # checks if the bot has been @ed
     if message.content == f'<@!{client.user.id}>':
-        await message.author.send('Hi, I am ...')
-        await message.channel.send(f'<@!{message.author.id}> Check your DM\'s')
+        await message.author.send('**Hi, I\'m DiscMatch! Thank you for reaching out** :wink: '
+                                  '\n*You\'re moments away from being matched with people !*  :eyes:')
+        message = await message.channel.send(f'<@!{message.author.id}> Check your DM\'s')
+        await message.add_reaction(':eyes:')
 
 client.run(TOKEN)
